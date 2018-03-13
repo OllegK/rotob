e601147c239ff0bcdd9d972174cbd407815e1425
ok <oleg.koleskin@gmail.com>
2018-03-13 23:15:33 +0200

    WIP10

--- arc.sh
+++ arc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rar a -r -hp -x*.env -x*/src/symbols.js -x*state.json -x*.editorconfig -x*.eslintrc.json -x*.gitignore -x*.log -x*/.vscode -x*/.git -x*/node_modules robot_$(date +%F_%R).rar ./
+rar a -r -hp -x*.env x*/src/config.js -x*/src/symbols.js -x*state.json -x*.editorconfig -x*.eslintrc.json -x*.gitignore -xna*.log -x*/.vscode -x*/.git -x*/node_modules robot_$(date +%F_%R).rar ./
